4e17079216b458dc57f6c84f220f8361cb52b9bc
Philip Loa <boyet007@gmail.com>
2018-08-17 22:27:32 +0700

    fix crud factory

--- database/factories/CrudFactory.php
+++ database/factories/CrudFactory.php
@@ -4,10 +4,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Crud::class, function (Faker $faker) {
     
-    $color = array('green', 'red');
-    $rand_keys = array_rand($color, 1);
+    // $color = array('green', 'red');
+    // $rand_keys = array_rand($color, 1);
+    // return [
+    //     'name' => $faker->name,
+    //     'color' => $color[$rand_keys]
+    // ];
+
     return [
-        'name' => $faker->name,
-        'color' => $color[$rand_keys]
+        'name' => $faker->lexify('???????'),
+        'color' => $faker->boolean ? 'red' : 'green'
     ];
 });
